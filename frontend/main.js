0a4bf63dc9f4c30c182c28fc52b7e09862706f50
sgunyon <>
2014-06-09 18:05:06 -0400

    Added css styling to map, output variables in a form, added update map function.

--- frontend/main.js
+++ frontend/main.js
@@ -8,78 +8,84 @@ var regressionConstant = 0.3638054;
 var mapblueAPI = "http://totaltrash.org/mapblue/lookup"
 var stateHouseLatitude = 39.768732;
 var stateHouseLongitude = -86.162612;
-var features = [];
 
-var roadStyle = {
-    strokeColor: "#FFFF00",
-    strokeWeight: 1,
-    strokeOpacity: 0.75
-};
-
-var parcelStyle = {
-    strokeColor: "#FF7800",
-    strokeOpacity: 1,
-    strokeWeight: 1,
-    fillColor: "#46461F",
-    fillOpacity: 0.25
-};
-
-// var infowindow = new google.maps.InfoWindow();
-
-function getDemPercentage(over18, black, hispanic, otherRace, unmarried,
-        childless) {
+function getDemProbability(over18, black, hispanic, otherRace, unmarried,
+                           childless) {
     if (over18 == 0) {
         return 0;
     }
 
     return regressionConstant +
-            (blackCoeff * (black / over18)) +
-            (hispanicCoeff * (otherRace / over18)) +
-            (otherRaceCoeff * (otherRace / over18)) +
-            (unmarriedCoeff * (unmarried / over18)) +
-            (childlessCoeff * (childless / over18));
+        (blackCoeff * (black / over18)) +
+        (hispanicCoeff * (hispanic / over18)) +
+        (otherRaceCoeff * (otherRace / over18)) +
+        (unmarriedCoeff * (unmarried / over18)) +
+        (childlessCoeff * (childless / over18));
 }
 
-function handleJSONResponse(data) {
-    if (features.length > 0) {
-        $.each(features, function(index, feature) {
-            map.data.remove(feature);
-        });
+function buildAPIURL(lat1, lon1, lat2, lon2) {
+    return mapblueAPI + "?lat1=" + lat1 +
+                        "&lon1=" + lon1 +
+                        "&lat2=" + lat2 +
+                        "&lon2=" + lon2;
+}
+
+function styleBlock(block) {
+    var over18 = block.getProperty('over18');
+    var black = block.getProperty('black');
+    var hispanic = block.getProperty('hispanic');
+    var otherRace = block.getProperty('otherRace');
+    var unmarried = block.getProperty('unmarried');
+    var childless = block.getProperty('childless');
+    var demProbability = getDemProbability(
+        block.getProperty('over18'),
+        block.getProperty('black'),
+        block.getProperty('hispanic'),
+        block.getProperty('otherRace'),
+        block.getProperty('unmarried'),
+        block.getProperty('childless')
+    )
+
+    if (over18 < .3) {
+        return {
+            fillColor: "#BBBBBB",
+            fillOpacity: .5,
+            strokeWeight: 1
+        };
     }
 
-    $.each(data, function(key, blocks) {
-        $.each(blocks, function(index, block) {
-            var newFeatures = map.data.addGeoJson(block);
+    if (demProbability < .5) {
+        return {
+            fillColor: "#BB4444",
+            fillOpacity: demProbability,
+            strokeWeight: 1
+        };
+    }
 
-            $.each(newFeatures, function(index, newFeature) {
-                map.data.overrideStyle(newFeature, {
-                    fillColor: "#4488CC",
-                    fillOpacity: getDemPercentage(
-                            block.properties.over18,
-                            block.properties.black,
-                            block.properties.hispanic,
-                            block.properties.otherRace,
-                            block.properties.unmarried,
-                            block.properties.childless
-                            )
-                });
-            });
-            features = features.concat(newFeatures);
-        });
+    return {
+        fillColor: "#4488CC",
+        fillOpacity: demProbability,
+        strokeWeight: 1
+    };
+}
+
+function restyleBlocks() {
+    
+    
+    
+    map.data.forEach(function(feature) {
+        overrideStyle(feature, styleBlock(feature));
     });
 }
 
-function loadBlocks() {
+function updateBlocks() {
     var bounds = map.getBounds();
     var northEast = bounds.getNorthEast();
     var southWest = bounds.getSouthWest();
 
-    $.getJSON(mapblueAPI, {
-        lat1: northEast.lat(),
-        lon1: southWest.lng(),
-        lat2: southWest.lat(),
-        lon2: northEast.lng()
-    }, handleJSONResponse);
+    map.data.loadGeoJson(buildAPIURL(
+        northEast.lat(), northEast.lng(), southWest.lat(), southWest.lng()
+    ))
 }
 
 function init() {
@@ -95,102 +101,15 @@ function init() {
 
     map = new google.maps.Map(document.getElementById('map'), {
         center: new google.maps.LatLng(latitude, longitude),
-        zoom: 16
+        zoom: 16,
+        minZoom: 14,
+        maxZoom: 18,
+        streetViewControl: false
     });
 
-    google.maps.event.addListener(map, 'bounds_changed', loadBlocks);
-}
+    map.data.setStyle(styleBlock);
 
-function clearMap() {
-    if (!currentFeature_or_Features)
-        return;
-    if (currentFeature_or_Features.length) {
-        for (var i = 0; i < currentFeature_or_Features.length; i++) {
-            if (currentFeature_or_Features[i].length) {
-                for (var j = 0; j < currentFeature_or_Features[i].length; j++) {
-                    currentFeature_or_Features[i][j].setMap(null);
-                }
-            }
-            else {
-                currentFeature_or_Features[i].setMap(null);
-            }
-        }
-    } else {
-        currentFeature_or_Features.setMap(null);
-    }
-    if (infowindow.getMap()) {
-        infowindow.close();
-    }
-}
-function showFeature(geojson, style) {
-    clearMap();
-    currentFeature_or_Features = new GeoJSON(geojson, style || null);
-    if (currentFeature_or_Features.type && currentFeature_or_Features.type == "Error") {
-        document.getElementById("put_geojson_string_here").value = currentFeature_or_Features.message;
-        return;
-    }
-    if (currentFeature_or_Features.length) {
-        for (var i = 0; i < currentFeature_or_Features.length; i++) {
-            if (currentFeature_or_Features[i].length) {
-                for (var j = 0; j < currentFeature_or_Features[i].length; j++) {
-                    currentFeature_or_Features[i][j].setMap(map);
-                    if (currentFeature_or_Features[i][j].geojsonProperties) {
-                        setInfoWindow(currentFeature_or_Features[i][j]);
-                    }
-                }
-            }
-            else {
-                currentFeature_or_Features[i].setMap(map);
-            }
-            if (currentFeature_or_Features[i].geojsonProperties) {
-                setInfoWindow(currentFeature_or_Features[i]);
-            }
-        }
-    } else {
-        currentFeature_or_Features.setMap(map)
-        if (currentFeature_or_Features.geojsonProperties) {
-            setInfoWindow(currentFeature_or_Features);
-        }
-    }
-
-    document.getElementById("put_geojson_string_here").value = JSON.stringify(geojson);
-}
-function rawGeoJSON() {
-    clearMap();
-    currentFeature_or_Features = new GeoJSON(JSON.parse(document.getElementById("put_geojson_string_here").value));
-    if (currentFeature_or_Features.length) {
-        for (var i = 0; i < currentFeature_or_Features.length; i++) {
-            if (currentFeature_or_Features[i].length) {
-                for (var j = 0; j < currentFeature_or_Features[i].length; j++) {
-                    currentFeature_or_Features[i][j].setMap(map);
-                    if (currentFeature_or_Features[i][j].geojsonProperties) {
-                        setInfoWindow(currentFeature_or_Features[i][j]);
-                    }
-                }
-            }
-            else {
-                currentFeature_or_Features[i].setMap(map);
-            }
-            if (currentFeature_or_Features[i].geojsonProperties) {
-                setInfoWindow(currentFeature_or_Features[i]);
-            }
-        }
-    } else {
-        currentFeature_or_Features.setMap(map);
-        if (currentFeature_or_Features.geojsonProperties) {
-            setInfoWindow(currentFeature_or_Features);
-        }
-    }
-}
-function setInfoWindow(feature) {
-    google.maps.event.addListener(feature, "click", function(event) {
-        var content = "<div id='infoBox'><strong>GeoJSON Feature Properties</strong><br />";
-        for (var j in this.geojsonProperties) {
-            content += j + ": " + this.geojsonProperties[j] + "<br />";
-        }
-        content += "</div>";
-        infowindow.setContent(content);
-        infowindow.setPosition(event.latLng);
-        infowindow.open(map);
-    });
+    google.maps.event.addListener(map, 'tilesloaded', updateBlocks);
+    google.maps.event.addListener(map, 'zoom_changed', updateBlocks);
+    google.maps.event.addListener(map, 'dragend', updateBlocks);
 }
