995e62363dd005170def4860d997550cdc6a21b1
Charlie Gunyon <charles.gunyon@gmail.com>
2014-06-14 10:53:08 -0400

    Display block stats on mouseover
    Some click handling is there too; eventually gonna keep track of all
    selected blocks and display total votes for them.

--- frontend/js/app/main.js
+++ frontend/js/app/main.js
@@ -10,29 +10,50 @@ var mapblueAPI = 'http://mapblue.org/lookup'
 var stateHouseLatitude = 39.768732;
 var stateHouseLongitude = -86.162612;
 var mapboxTileJSON = 'https://a.tiles.mapbox.com/v3/examples.map-20v6611k,mapbox.dc-property-values.jsonp?secure';
+var blockIDs = [];
 
-function getDemProbability(over18, black, hispanic, otherRace, unmarried,
-                           childless) {
+function calculateBlockStatistics(block) {
+    var over18 = block.properties.over18;
+    var black = block.properties.black;
+    var hispanic = block.properties.hispanic;
+    var otherRace = block.properties.otherRace;
+    var nonWhite = black + hispanic + otherRace;
+    var white = over18 - nonWhite;
+    var unmarried = block.properties.unmarried;
+    var childless = block.properties.childless;
+
     if (over18 == 0) {
-        return 0;
+        block.properties.white = 0;
+        block.properties.blackPct = 0;
+        block.properties.hispanicPct = 0;
+        block.properties.otherRacePct = 0;
+        block.properties.whitePct = 0;
+        block.properties.unmarriedPct = 0;
+        block.properties.childlessPct = 0;
+        block.properties.demPct = 0;
+        block.properties.democrat = 0;
+        return;
     }
 
-    return regressionConstant + (blackCoeff * (black / over18)) +
-                                (hispanicCoeff * (hispanic / over18)) +
-                                (otherRaceCoeff * (otherRace / over18)) +
-                                (unmarriedCoeff * (unmarried / over18)) +
-                                (childlessCoeff * (childless / over18));
-}
-
-function getBlockDemProbability(block) {
-    return getDemProbability(
-        block.properties.over18,
-        block.properties.black,
-        block.properties.hispanic,
-        block.properties.otherRace,
-        block.properties.unmarried,
-        block.properties.childless
-    );
+    block.properties.white = white;
+    block.properties.blackPct = black / over18;
+    block.properties.hispanicPct = hispanic / over18;
+    block.properties.otherRacePct = otherRace / over18;
+    block.properties.whitePct = white / over18;
+    block.properties.unmarriedPct = unmarried / over18;
+    block.properties.childlessPct = childless / over18;
+    block.properties.demPct = regressionConstant +
+        (blackCoeff     * block.properties.blackPct)     +
+        (hispanicCoeff  * block.properties.hispanicPct)  +
+        (otherRaceCoeff * block.properties.otherRacePct) +
+        (unmarriedCoeff * block.properties.unmarriedPct) +
+        (childlessCoeff * block.properties.childlessPct);
+    if (block.properties.demPct < .50) {
+        block.properties.democrat = -(over18 * block.properties.demPct);
+    }
+    else {
+        block.properties.democrat = over18 * block.properties.demPct;
+    }
 }
 
 function buildAPIURL(lat1, lon1, lat2, lon2) {
@@ -60,37 +81,36 @@ function getMapCoordinates() {
 }
 
 function blockStyler(block) {
-    var demProbability = getBlockDemProbability(block);
+    if (typeof block.properties.clicked == "undefined") {
+        block.properties.clicked = false;
+    }
+
+    calculateBlockStatistics(block);
 
     if (block.properties.over18 == 0) {
         return {
             fillColor: '#FFFFFF',
             fillOpacity: 0.0,
-            stroke: false
+            stroke: block.properties.clicked,
+            weight: 2
         };
     }
 
-    if (demProbability < .5) {
+    if (block.properties.demPct < .5) {
         return {
             fillColor: '#BB4444',
-            fillOpacity: 1.0 - demProbability,
-            stroke: false
+            fillOpacity: 1.0 - block.properties.demPct,
+            stroke: block.properties.clicked,
+            weight: 2
         };
     }
 
     return {
         fillColor: '#4488CC',
-        fillOpacity: demProbability,
-        stroke: false
+        fillOpacity: block.properties.demPct,
+        stroke: block.properties.clicked,
+        weight: 2
     };
-}
-
-function clearBlocks() {
-    var map = getMap();
-
-    if (geoJSONData && map.hasLayer(geoJSONLayer)) {
-        map.removeLayer(geoJSONLayer);
-    }
 }
 
 function updateCoefficients() {
@@ -103,28 +123,75 @@ function updateCoefficients() {
 }
 
 function setBlocks() {
-    geoJSONLayer = L.geoJson(geoJSONData, {
-        style: blockStyler
-    });
-    geoJSONLayer.addTo(getMap());
+    if (geoJSONLayer) {
+        geoJSONLayer.addData(geoJSONData);
+    }
 }
 
 function loadBlocks() {
     $.getJSON(mapblueAPI, getMapCoordinates(), function(data) {
         geoJSONData = data;
-        reloadBlocks();
+        setBlocks();
     });
 }
 
 function reloadBlocks(e) {
     updateCoefficients();
-    clearBlocks();
     setBlocks();
+    console.log(geoJSONLayer);
+}
+
+function blockFilter(block) {
+    if (blockIDs.indexOf(block.id) == -1) {
+        blockIDs.push(block.id);
+        return true;
+    }
+    return false;
+}
+
+function blockClicked(e) {
+    if (e.target.feature.properties.clicked) {
+        e.target.feature.properties.clicked = false;
+    }
+    else {
+        e.target.feature.properties.clicked = true;
+    }
+    e.target.setStyle({
+        stroke: e.target.feature.properties.clicked
+    });
+    if (!L.Browser.ie && !L.Browser.opera) {
+        layer.bringToFront();
+    }
+}
+
+function toPercent(s) {
+    return Math.round(s * 100)
+}
+
+function blockMousedOver(e) {
+    var ps = e.target.feature.properties;
+
+    console.log(ps);
+
+    $('#block_name').html(ps.name);
+    $('#block_population').html(ps.over18);
+    $('#block_black')
+        .html(toPercent(ps.blackPct) + "% (" + Math.round(ps.black) + ")");
+    $('#block_hispanic')
+        .html(toPercent(ps.hispanicPct) + "% (" + Math.round(ps.hispanic) + ")");
+    $('#block_other_race')
+        .html(toPercent(ps.otherRacePct) + "% (" + Math.round(ps.otherRace) + ")");
+    $('#block_white')
+        .html(toPercent(ps.whitePct) + "% (" + Math.round(ps.white) + ")");
+    $('#block_unmarried')
+        .html(toPercent(ps.unmarriedPct) + "% (" + Math.round(ps.unmarried) + ")");
+    $('#block_childless')
+        .html(toPercent(ps.childlessPct) + "% (" + Math.round(ps.childless) + ")");
+    $('#block_democratic')
+        .html(toPercent(ps.demPct) + "% (" + Math.round(ps.democrat) + ")");
 }
 
 function init() {
-    // $(function() {$('body')});
-
     $('#blackCoeff').val(blackCoeff);
     $('#hispanicCoeff').val(hispanicCoeff);
     $('#otherRaceCoeff').val(otherRaceCoeff);
@@ -132,22 +199,25 @@ function init() {
     $('#childlessCoeff').val(childlessCoeff);
     $('#regressionConstant').val(regressionConstant);
 
-    // map = L.mapbox.map('map', mapboxTileJSON, {
     $('#map').mapbox(mapboxTileJSON, {
         center: [stateHouseLatitude, stateHouseLongitude],
         zoom: 16,
-        minZoom: 14,
         maxZoom: 18,
-    }).on('moveend', loadBlocks);;
+    }).on('moveend', loadBlocks);
+
+    geoJSONLayer = L.geoJson(geoJSONData, {
+        style: blockStyler,
+        filter: blockFilter,
+        onEachFeature: function(block, layer) {
+            layer.on({
+                click: blockClicked,
+                mouseover: blockMousedOver
+            })
+        }
+    });
+    geoJSONLayer.addTo(getMap());
     loadBlocks();
 }
 
 $(document).ready(init);
-
-/*
-require([
-    'json3', 'jquery', 'mapbox', 'mapbox.jquery', 'mapbox.jquery.geocoder',
-    'mapbox.share', 'app/menu.jquery'
-], init);
-*/
 
